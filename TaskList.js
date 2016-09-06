a2c7b738a65e1700ef92af7d57d5c0348593c8ba
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2016-09-06 16:39:42 +0200

    created TaskList with ListView

---
+++ TaskList.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  ListView
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40
+  }
+});
+
+class TaskList extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.state = {
+      dataSource: ds.cloneWithRows(props.todos)
+    };
+  }
+  renderRow(todo) {
+    return (
+      <Text>{todo.task}</Text>
+    );
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView
+          key={this.props.todos}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+        />
+
+      </View>
+    );
+  }
+
+}
+
+TaskList.propTypes = {
+  todos: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+};
+
+export default TaskList;
