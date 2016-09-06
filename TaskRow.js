063bbb8969560df14698f9b43d4b55cca00513e8
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2016-09-06 17:03:53 +0200

    styled text row

--- TaskRow.js
+++ TaskRow.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import {
-  Text
+  Text,
+  View,
+  StyleSheet
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#e7e7e7',
+    padding: 20,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20
+  }
+});
 
 class TaskRow extends React.Component {
   render() {
     return (
-      <Text>{this.props.todo.task}</Text>
+      <View style={styles.container}>
+        <Text>{this.props.todo.task}</Text>
+      </View>
     );
   }
 }
